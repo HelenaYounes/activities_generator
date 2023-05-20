e7ff2fefdafcf1f849af1b8752ed0ae70a0162e4
Héléna Younes <younesq.helena@gmail.com>
2023-05-20 18:49:05 +0200

    added .env file, and config/db to instantiate mongoose connection

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
-
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import routes from "./routes/index.js";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(morgan("dev"));
-app.use(bodyParser.json())
-app.use(express.urlencoded({extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
@@ -22,6 +22,7 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.message);
 });
 
-app.listen(3000, () => {
-  console.log("listening port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
